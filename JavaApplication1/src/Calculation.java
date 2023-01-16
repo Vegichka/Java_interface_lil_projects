9f6e71bdaf2b0b8f5abeca186d27c49cb25ad396
Vegichka <120520031+Vegichka@users.noreply.github.com>
2023-01-17 00:11:34 +0100

    Add files via upload

---
+++ JavaApplication1/src/Calculation.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author domina
+ */
+public class Calculation {
+    
+    public int num1, num2, tot;
+    
+    public int sum(){
+        tot = num1 + num2;
+        return tot;
+    }
+    
+}
