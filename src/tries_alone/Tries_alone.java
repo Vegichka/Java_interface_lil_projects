8b7442fcf5a1ba6f97980e8d9f7eb59848982b02
Vegichka <120520031+Vegichka@users.noreply.github.com>
2023-01-19 14:40:19 +0100

    Add files via upload

---
+++ src/tries_alone/Tries_alone.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tries_alone;
+
+/**
+ *
+ * @author domina
+ */
+public class Tries_alone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
